169fdf2fc28cba69bb08f866c67a980c2687dbaa
Ladislav Misurák <lacee677@gmail.com>
2019-11-01 13:03:46 +0100

    add test facebook value

--- _assets/javascripts/jegyek.js
+++ _assets/javascripts/jegyek.js
@@ -121,7 +121,7 @@ if ($(".ticket-form").length > 0) {
   window.fbAsyncInit = function () {
     // initialize facebook
     FB.init({
-      appId: '267323596708516',
+      appId: '451186618936553',
       xfbml: true,
       status: true,
       version: 'v2.9'
